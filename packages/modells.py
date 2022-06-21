619a9f738742df301a6610a84d20797812639043
Pavlo Yerebakan <ppvr3407@gmail.com>
2022-06-21 21:32:36 +0300

    временно

--- packages/modells.py
+++ packages/modells.py
@@ -1,6 +1,7 @@
 from packages import db, UserMixin
 from datetime import datetime
 
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     login = db.Column(db.String(120), nullable=False, unique=True)
